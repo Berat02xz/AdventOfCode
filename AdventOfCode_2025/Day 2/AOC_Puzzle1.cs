59976bc9b59336ee48a253f266f29637b5bebf6e
BeratAhmetaj <beratahmetaj02@gmail.com>
2025-12-01 17:42:28 +0100

    env ready for day 2

---
+++ AdventOfCode_2025/Day 2/AOC_Puzzle1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode_2025.Day_2
+{
+    public class AOC_Puzzle2
+    {
+        public static void Solution(string[] args)
+        {
+            var lines = File.ReadAllLines("../../../Day 2/input.txt")
+                               .Where(l => !string.IsNullOrWhiteSpace(l))
+                               .ToList();
+
+            
+
+        }
+
+    }
+}
--- AdventOfCode_2025/Start.cs
+++ AdventOfCode_2025/Start.cs
@@ -3,6 +3,7 @@
 
 
 using AdventOfCode_2025.Day_1;
+using AdventOfCode_2025.Day_2;
 
 namespace AdventOfCode_2025
 {
@@ -10,18 +11,11 @@ namespace AdventOfCode_2025
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Pick Solution From Commented Lines To Run (Open in editor)");
+            Console.WriteLine("Advent Of Code Solution:");
             // FINKI_Puzzle1.Solution(args);
-            AOC_Puzzle1.Solution(args);
-            // Puzzle3.Solution(args);
-            // Puzzle4.Solution(args);
-            // Puzzle5.Solution(args);
-            // Puzzle6.Solution(args);
-            // Puzzle7.Solution(args);
-            // Puzzle8.Solution(args);
-            // Puzzle9.Solution(args);
-            // Puzzle10.Solution(args);
-            // Puzzle11.Solution(args);
+            // AOC_Puzzle1.Solution(args);
+            AOC_Puzzle2.Solution(args);
+
 
         }
     }
